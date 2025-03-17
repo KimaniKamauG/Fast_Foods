94cbbcacba72c702688fe171e6a1b3b9a089b85f
KimaniKamauG <143401342+KimaniKamauG@users.noreply.github.com>
2025-03-17 21:23:07 +0300

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from rest_framework import APIView 
+from rest_framework.response import Response 
+from django.contrib.gis.db.models.functions import Distance 
+
+# Create your views here.
+class RestaurantAPI(APIView):
+    def post(self, request):
+        lat = request.data.get('lat')
+        lng = request.data.get('lng')
+        point = Point(float(lng), float(lat), srid=4326)
+
+        restaurants = MuhindiFastFoods.objects.annotate(
+            distance=Distance('location', point)
+        ).filter(
+            location__distance_lte=(point, models.F('delivery_radius'))
+        ).order_by('ditance')
+
+        return Response({
+            'restaurants': [
+                {
+                    'name':r.name,
+                    'address': r.address,
+                    'distance': r.distance.m
+                }
+                for r in restaurants
+            ]
+        })
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    preparation_time = models.PositiveIntegerField(help_text='Minutes')
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('Preparing', 'Preparing'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered'),
+        ('Cancelled', 'Cancelled'),
+    ]
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField('OrderItem')
+    total = models.DecimaField(max_digits=8, decimal_places=2)
+    delivery_address = models.PointField(geography=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='Preparing')
