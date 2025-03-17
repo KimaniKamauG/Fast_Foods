8512a1c47053b2adb0542f0970e4734d6f6a70b5
KimaniKamauG <143401342+KimaniKamauG@users.noreply.github.com>
2025-03-17 19:47:48 +0300

    Initial commit

---
+++ users/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password):
+        if not email:
+            raise ValueError('Users must have an email address!')
+        if not password:
+            raise ValueError('A password is required for all users!')
+        user = self.model(email=self.normalise_email(email), username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user 
+    
+    def create_suoeruser(self, email, username, password):
+        user = self.create_user(email, username, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user 
+    
+class User(AbstractUser):
+    email = models.EmailField(unique=True, max_length=255)
+    username = models.CharField(unique=True, max_length=20)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
