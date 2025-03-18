e5a54ccffb5500f028fa9c984b30c04a9db79192
KimaniKamauG <143401342+KimaniKamauG@users.noreply.github.com>
2025-03-18 15:10:30 +0300

    Initial commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from restaurant.models import MenuItem 
+from orders.models import OrderItem, Order
 from django.contrib.auth import get_user_model 
 
 User = get_user_model() 
@@ -9,4 +11,18 @@ class UserSerializer(serializers.ModelSerializer):
         model = User 
         fields = ['id', 'email', 'username']
 
-        
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem 
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem 
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'items', 'total_price', 'status', 'delivery_address', 'created_at']
