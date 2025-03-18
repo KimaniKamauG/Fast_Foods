29a5ac8c2fa9f7a03949a220c0e2e6b44adde417
KimaniKamauG <143401342+KimaniKamauG@users.noreply.github.com>
2025-03-18 15:13:51 +0300

    Initial commit

--- orders/models.py
+++ orders/models.py
@@ -13,7 +13,8 @@ class Order(models.Model):
     total_price = models.DecimalField(max_digits=6, decimal_places=2)
     status = models.CharField(max_length=20, default='Pending')
     delivery_address = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'Order {self.id} by {self.user}'
